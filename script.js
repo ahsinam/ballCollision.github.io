7ae8efa636651097c80222bcdee732d9dc012040
ahsinam171 <ahsinam171@gmail.com>
2023-05-17 21:32:57 +0545

    many balls movement

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
 class BallCollisionGame {
-  constructor(x, y, dx, dy) {
+  constructor(id, x, y, dx, dy) {
+    this.id = id;
     this.x = x;
     this.y = y;
     this.dx = dx;
     this.dy = dy;
   }
 
-  generateRandomNumber = (maxLimit = 600) => {
+  generateRandomNumber = (maxLimit = 200) => {
     let xRandom = Math.floor(Math.random() * maxLimit);
     let yRandom = Math.floor(Math.random() * maxLimit);
     return { xRandom, yRandom };
   };
 
-  drawBall = (x, y) => {
+  drawBall = (x, y, dx, dy) => {
     const mainContainer = document.getElementById("container");
     const oneBall = document.createDocumentFragment();
     const ballElement = document.createElement("div");
     ballElement.classList.add("ball");
+    ballElement.setAttribute("id", this.id);
     ballElement.style.left = x + "px";
     ballElement.style.top = y + "px";
+
     oneBall.appendChild(ballElement);
     mainContainer.appendChild(oneBall);
   };
 
   moveBall = () => {
     const mainContainer = document.getElementById("container");
-    const ball = document.querySelector("#ball");
-    mainContainer.removeChild(document.querySelector(".ball"));
+    // const ball = document.querySelector("#ball");
+    const ball = document.getElementById(this.id);
+    // mainContainer.removeChild(document.querySelector(".ball"));
+    ball.remove();
     const containerWidth = mainContainer.offsetWidth;
     const containerHeight = mainContainer.offsetHeight;
     if (this.x + this.dx + 40 > containerWidth || this.x < 0) {
@@ -44,12 +49,20 @@ class BallCollisionGame {
   init = () => {
     this.drawBall(this.x, this.y);
     setInterval(() => {
-      //   this.moveBall(this.x, this.y, this.dx, this.dy);
-      this.generateRandomNumber();
       this.moveBall();
     }, 100);
   };
 }
 
-const ball = new BallCollisionGame(50, 50, 5, 5);
-ball.init();
+const ball1 = new BallCollisionGame(1, 50, 50, 7, 5);
+const ball2 = new BallCollisionGame(2, 250, 400, 4, 8);
+const ball3 = new BallCollisionGame(3, 20, 200, 1, 4);
+const ball4 = new BallCollisionGame(4, 60, 90, 9, 2);
+ball1.init();
+ball2.init();
+ball3.init();
+ball4.init();
+
+// requestAnimationFrame(() => {
+//   this.moveBall();
+// });
