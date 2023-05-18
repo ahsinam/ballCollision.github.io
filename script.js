02c0b446c939154d2211e666bc627a084eae3d92
ahsinam171 <ahsinam171@gmail.com>
2023-05-18 09:49:14 +0545

    Collision

--- script.js
+++ script.js
@@ -21,51 +21,6 @@ const rotate = (velocityX, velocityY, angle) => {
   return rotatedVelocities;
 };
 
-function resolveCollision(particle, otherParticle) {
-  const xVelocityDiff = particle.dx - otherParticle.dx;
-  const yVelocityDiff = particle.dy - otherParticle.dy;
-
-  const xDist = otherParticle.x - particle.x;
-  const yDist = otherParticle.y - particle.y;
-
-  // Prevent accidental overlap of particles
-  if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
-    // Grab angle between the two colliding particles
-    const angle = -Math.atan2(
-      otherParticle.y - particle.y,
-      otherParticle.x - particle.x
-    );
-
-    // Store mass in var for better readability in collision equation
-    const m1 = particle.mass;
-    const m2 = otherParticle.mass;
-
-    // Velocity before equation
-    const u1 = rotate(particle.dx, particle.dy, angle);
-    const u2 = rotate(otherParticle.dx, otherParticle.dy, angle);
-
-    const v1 = {
-      x: (u1.x * (m1 - m2)) / (m1 + m2) + (u2.x * 2 * m2) / (m1 + m2),
-      y: u1.y,
-    };
-    const v2 = {
-      x: (u2.x * (m1 - m2)) / (m1 + m2) + (u1.x * 2 * m2) / (m1 + m2),
-      y: u2.y,
-    };
-
-    // Final velocity after rotating axis back to original location
-    const vFinal1 = rotate(v1, -angle);
-    const vFinal2 = rotate(v2, -angle);
-
-    // Swap particle velocities for realistic bounce effect
-    particle.dx = vFinal1.x;
-    particle.dy = vFinal1.y;
-
-    otherParticle.dx = vFinal2.x;
-    otherParticle.dy = vFinal2.y;
-  }
-}
-
 const balls = [
   {
     x: generateRandomNumber().xRandom,
@@ -138,22 +93,6 @@ const balls = [
     mass: 10,
   },
 ];
-// const balls = [
-//   {
-//     x: generateRandomNumber().xRandom,
-//     y: generateRandomNumber().yRandom,
-//     dx: 3,
-//     dy: 2,
-//     mass: 1,
-//   },
-//   {
-//     x: generateRandomNumber().xRandom,
-//     y: generateRandomNumber().yRandom,
-//     dx: 2,
-//     dy: 4,
-//     mass: 2,
-//   },
-// ];
 const ballInstances = [];
 
 class BallCollisionGame {
@@ -191,15 +130,12 @@ class BallCollisionGame {
     const xDist = otherParticle.x - this.x;
     const yDist = otherParticle.y - this.y;
 
-    // Prevent accidental overlap of particles
     if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
-      // Grab angle between the two colliding particles
       const angle = -Math.atan2(
         otherParticle.y - this.y,
         otherParticle.x - this.x
       );
 
-      // Store mass in var for better readability in collision equation
       const m1 = this.mass;
       const m2 = otherParticle.mass;
 
@@ -216,25 +152,16 @@ class BallCollisionGame {
         y: u2.y,
       };
 
-      // Final velocity after rotating axis back to original location
       const vFinal1 = rotate(v1.x, v1.y, -angle);
 
       const vFinal2 = rotate(v2.x, v2.y, -angle);
 
-      // Swap particle velocities for realistic bounce effect
-      console.log(vFinal1, vFinal2);
       this.dx = vFinal1.x;
       this.dy = vFinal1.y;
 
       otherParticle.dx = vFinal2.x;
       otherParticle.dy = vFinal2.y;
-
-      // particle.x += otherParticle.x;
-      // particle.y += otherParticle.y;
-
-      // otherParticle.x += otherParticle.dx;
-      // otherParticle.y += otherParticle.dy;
-      // console.log(otherParticle, particle);
+      clearInterval();
     }
   };
 
@@ -284,16 +211,3 @@ balls.forEach((v, index) => {
   ballInstances.push(ball);
   ball.init();
 });
-
-// const ball1 = new BallCollisionGame(1, 50, 50, 7, 5);
-// const ball2 = new BallCollisionGame(2, 250, 400, 4, 8);
-// const ball3 = new BallCollisionGame(3, 20, 200, 1, 4);
-// const ball4 = new BallCollisionGame(4, 60, 90, 9, 2);
-// ball1.init();
-// ball2.init();
-// ball3.init();
-// ball4.init();
-
-// requestAnimationFrame(() => {
-//   this.moveBall();
-// });
